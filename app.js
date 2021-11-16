80a5678bfa192d8bece7894313d10fd5836d313f
Joshua Osburn <joshuakosburn@gmail.com>
2021-11-16 17:29:58 -0500

    Added asynchronous await to db connect

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 // Load our config as soon as possible
 require('dotenv').config({ path: './config.env' });
 require('express-async-errors');
+
 const express = require('express');
 const app = express();
 
 const connectToDB = require('./db/connect');
+
+// ROUTERS
+const authRouter = require('./routes/auth');
+const jobRouter = require('./routes/jobs');
 
 // Error Handler Middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -15,9 +20,8 @@ app.use(express.json());
 // extra packages
 
 // ROUTES
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobRouter);
 
 // SETUP ERROR HANDLER MIDDLEWARE
 app.use(notFoundMiddleware);
@@ -27,7 +31,7 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    connectToDB(process.env.MONGO_URI);
+    await connectToDB(process.env.MONGO_URI).then(console.log("Database connected!"));
     app.listen(port, () => console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
